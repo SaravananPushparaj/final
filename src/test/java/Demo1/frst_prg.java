e7aec4a14c334e87667b848465b23f9af20d9f0d
unknown <saravananpushparaj@gmail.com>
2015-09-01 23:30:55 +0530

    first commit

---
+++ src/test/java/Demo1/frst_prg.java
@@ -0,0 +1,15 @@
+package Demo1;
+
+import org.testng.annotations.Test;
+
+public class frst_prg {
+	
+	@ Test(groups={"SmokeTest"})
+	
+	public void display()
+	{
+		System.out.println("Hello World");
+	}
+	
+
+}
